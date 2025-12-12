94e189029a2569f961578cb94a4b6cb59a74117b
venkads94 <vandhanaveni@gmail.com>
2025-12-12 15:28:54 +0530

    chore: updating dependencies
    Updating pom.xml and TestRunner.java as per the version

---
+++ src/test/java/com/booking/runner/TestRunner.java
@@ -0,0 +1,8 @@
+package com.booking.runner;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "", glue = "", publish = true)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
